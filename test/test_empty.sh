72737f10e13657614d264fe675671b60b2999df1
Ralf Pannemans <ralf.pannemans@googlemail.com>
2018-09-16 18:36:19 +0200

    Add another integration test

--- test/test_empty.sh
+++ test/test_empty.sh
@@ -3,4 +3,5 @@ go build github.com/c0d1ngm0nk3y/sample-application-go
 ./sample-application-go &
 sleep 2
 curl -v -d @empty.json localhost:8080/quotation
+curl -v -d @simple.json localhost:8080/quotation
 killall sample-application-go
